2b336e718e0692ca733d6ace6afcf6555293029d
Boris V.Kuznetsov <socnetfpga@gmail.com>
2019-02-03 14:18:53 +0300

    Implemented IO. Implemented abstractions

--- build.sbt
+++ build.sbt
@@ -2,6 +2,19 @@ name := "interview"
 version := "1.0"
 scalaVersion := "2.12.8"
 
+libraryDependencies ++= Seq(
+  // Main
+  "org.scalaz"    %% "scalaz-core"            % "7.3.0-M27",
+  "org.scalaz"    %% "scalaz-effect"          % "7.3.0-M27",
+  "org.scalaz"    %% "scalaz-concurrent"      % "7.3.0-M27",
+  //"io.argonaut"   %% "argonaut"               % "6.2.2",
+  //"org.scala-stm" %% "scala-stm"              % "0.8",
+  "org.scalacheck"%% "scalacheck"             % "1.14.0" % "test",
+  "org.specs2"    %% "specs2-core"            % "4.3.5"  % "test",
+)
+
+scalacOptions in Test ++= Seq("-Yrangepos")
+
 scalacOptions in Test ++= Seq("-Yrangepos")
 
 // Lint
--- src/main/scala/task0/Main.scala
+++ src/main/scala/task0/Main.scala
@@ -1,10 +1,57 @@
 
 package task0
 
-object task extends App {
+import scalaz._
+import Scalaz._
+import effect._
+import IO._
 
-  println ("Hello")
+
+
+sealed abstract class Currency
+sealed abstract class Security
+sealed abstract class Trader
+
+
+final case object USD extends Currency
+final case object BuySide extends Trader
+final case object SellSide extends Trader
+
+final case object SecurityA extends Security
+final case object SecurityB extends Security
+final case object SecurityC extends Security
+final case object SecurityD extends Security
+
+
+object Task extends App {
+
+  val clientsFile = "/home/bku/work/git/scala/interview_scala/src/main/resources/clients.txt"
+  val ordersFile = "/home/bku/work/git/scala/interview_scala/src/main/resources/orders.txt"
+
+  val act0  = for {
+    _ <- putStrLn("Hello Scalaz")
+  } yield()
   
+  act0.unsafePerformIO
+  
+  // Wrap to IO monad
+  
+  val clientsIO  = IO {
+    //val source  = scala.io.Source.fromFile("/resources/clients.txt") // FIXME This doesn't work 
+    val source  = scala.io.Source.fromFile(clientsFile)
+    source.getLines.toStream
+  }
+  
+  val ordersIO  = IO {
+    //val source  = scala.io.Source.fromFile("/resources/clients.txt") // FIXME This doesn't work 
+    val source  = scala.io.Source.fromFile(ordersFile)
+    source.getLines.toStream
+  }
 
+  // Process input stream
+  val clients = clientsIO.unsafePerformIO.toList
+  val orders  = ordersIO.unsafePerformIO.toList
+  clients foreach (println)
+  orders  foreach (println)
 }
 
